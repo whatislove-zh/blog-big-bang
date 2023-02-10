0f93ae9dd92082942f85844cacd9884a01eb640a
Vladislav <vladgm233223@gmail.com>
2023-02-10 18:32:35 +0200

    initial settings

---
+++ src/pages/Feed.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Feed = () => {
+  return (
+    <div>Feed</div>
+  )
+}
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+export const Home = () => {
+  return <Typography align="center">Home</Typography>;
+};
