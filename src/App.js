1495d72b52cdc2041b618dacaabeff10adf0f082
Vladislav <vladgm233223@gmail.com>
2023-02-10 18:33:43 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import { MainLayout } from "./components/layout/MainLayout";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Registration } from "./pages/Registration";
+import { Feed } from "./pages/Feed";
+import { Profile } from "./pages/Profile";
+import { Post } from "./pages/Post";
+import { NotFound } from "./pages/NotFound";
+import { Container } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="feed" element={<Feed />} />
+          <Route path="feed/:slug" element={<Post />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="registration" element={<Registration />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Container>
   );
 }
 
