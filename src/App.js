e4c94a21f24125be021a554b96d48dcd5b4a2c71
Vladislav <vladgm233223@gmail.com>
2023-02-11 15:12:36 +0200

    remove Container to layout
    перемістив сонтейнер до layout, щоб не ламав бекграунд у login page

--- src/App.js
+++ src/App.js
@@ -7,23 +7,20 @@ import { Feed } from "./pages/Feed";
 import { Profile } from "./pages/Profile";
 import { Post } from "./pages/Post";
 import { NotFound } from "./pages/NotFound";
-import { Container } from "@mui/material";
 
 function App() {
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="feed" element={<Feed />} />
-          <Route path="feed/:slug" element={<Post />} />
-          <Route path="profile" element={<Profile />} />
-        </Route>
-        <Route path="login" element={<Login />} />
-        <Route path="registration" element={<Registration />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Container>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="feed" element={<Feed />} />
+        <Route path="feed/:slug" element={<Post />} />
+        <Route path="profile" element={<Profile />} />
+      </Route>
+      <Route path="login" element={<Login />} />
+      <Route path="sign-up" element={<Registration />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Footer } from "./Footer";
@@ -6,12 +6,12 @@ import { Header } from "./Header";
 
 export const MainLayout = () => {
   return (
-    <>
+    <Container>
       <Header />
       <Box sx={{minHeight:"85vh"}}>
         <Outlet />
       </Box>
       <Footer />
-    </>
+    </Container>
   );
 };
